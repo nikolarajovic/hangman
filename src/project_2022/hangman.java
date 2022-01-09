ca561abfda2f0f18441b5f82d0d84b43b9fd7f2c
Nikola Rajovic <nikola.rajovic1992@gmail.com>
2022-01-09 22:53:35 +0100

    Seperation method

--- src/project_2022/hangman.java
+++ src/project_2022/hangman.java
@@ -1,10 +1,26 @@
 package project_2022;
 
+import java.util.Scanner;
+
 public class hangman {
 
+	public static void seperation(String word, char[] letters) {
+		for (int i = 0; i < letters.length; i++) {
+			letters[i] = word.charAt(i);
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("*** WELCOME TO HANGMAN ***");
+		System.out.print("\nEnter the word: ");
+		String word = s.nextLine();
 
+		char[] letters = new char[word.length()];
+
+		boolean[] guessed = new boolean[word.length()];
+
+		seperation(word, letters);
 	}
 
 }
